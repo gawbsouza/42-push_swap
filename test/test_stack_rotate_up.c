f0b15d04854ff6521aa59e792b6b7df55bcc00bf
gasouza <gasouza@student.42sp.org.br>
2022-09-07 11:26:45 -0300

    test_stack_rotate_up: assert that "next" of last node is NULL

--- test/test_stack_rotate_up.c
+++ test/test_stack_rotate_up.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 10:35:28 by gasouza           #+#    #+#             */
-/*   Updated: 2022/09/07 11:02:42 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/09/07 11:26:02 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ TEST(stack_rotate_up, StackWithOneItem)
 	stack_rotate_up(stack);
 	TEST_ASSERT_NOT_NULL(stack->items);
 	TEST_ASSERT_EQUAL_INT(21, stack->items->value);
+	TEST_ASSERT_NULL(stack->items->next);
 }
 
 TEST(stack_rotate_up, StackWithTwoItems)
@@ -55,6 +56,7 @@ TEST(stack_rotate_up, StackWithTwoItems)
 	stack_rotate_up(stack);
 	TEST_ASSERT_EQUAL_INT(21, stack->items->value);
 	TEST_ASSERT_EQUAL_INT(42, stack->items->next->value);
+	TEST_ASSERT_NULL(stack->items->next->next);
 }
 
 TEST(stack_rotate_up, StackWithMultiItems)
@@ -70,6 +72,7 @@ TEST(stack_rotate_up, StackWithMultiItems)
 	TEST_ASSERT_EQUAL_INT(42, stack->items->value);
 	TEST_ASSERT_EQUAL_INT(21, stack->items->next->value);
 	TEST_ASSERT_EQUAL_INT(64, stack->items->next->next->value);
+	TEST_ASSERT_NULL(stack->items->next->next->next);
 }
 
 TEST_GROUP_RUNNER(stack_rotate_up)
