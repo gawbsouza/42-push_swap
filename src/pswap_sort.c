79c31b079b83692cbc0f3df9069b8e2ef3a28558
gasouza <gasouza@student.42sp.org.br>
2022-09-11 11:54:40 -0300

    pswap_sort: refactory

--- src/pswap_sort.c
+++ src/pswap_sort.c
@@ -6,26 +6,39 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 16:05:39 by gasouza           #+#    #+#             */
-/*   Updated: 2022/09/11 07:35:37 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/09/11 10:19:36 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	try_swap_a(t_pswap *pswap, char **log)
-{
-	t_node	*first;
-	t_node	*second;
+static void	try_rotate_up_a(t_pswap *pswap, char **log);
+static void	try_swap_a(t_pswap *pswap, char **log);
+static void	move_all_items_from_b_ordered(t_pswap *pswap, char **log);
 
-	if (!pswap || !log)
-		return ;
-	first = pswap->a->items;
-	second = first->next;
-	if (second && first->value > second->value)
+char	*pswap_sort(t_pswap *pswap)
+{
+	char	*log;
+	size_t	sentinel;
+	size_t	size;
+
+	log = NULL;
+	if (!pswap)
+		return (log);
+	sentinel = 1;
+	size = pswap->a->size;
+	while (sentinel <= size)
 	{
-		add_msg_to_log("sa\n", log);
-		sa(pswap);
+		if (!stack_pos_ordered(pswap->a, sentinel))
+		{
+			move_n_items_from_a(pswap, sentinel - 1, &log);
+			try_rotate_up_a(pswap, &log);
+			try_swap_a(pswap, &log);
+			move_all_items_from_b_ordered(pswap, &log);
+		}
+		sentinel++;
 	}
+	return (log);
 }
 
 static void	try_rotate_up_a(t_pswap *pswap, char **log)
@@ -39,8 +52,24 @@ static void	try_rotate_up_a(t_pswap *pswap, char **log)
 	last = pswap->a->last;
 	if (first->value > last->value)
 	{
-		add_msg_to_log("ra\n", log);
 		ra(pswap);
+		add_msg_to_log("ra\n", log);
+	}
+}
+
+static void	try_swap_a(t_pswap *pswap, char **log)
+{
+	t_node	*first;
+	t_node	*second;
+
+	if (!pswap || !log)
+		return ;
+	first = pswap->a->items;
+	second = first->next;
+	if (second && first->value > second->value)
+	{
+		sa(pswap);
+		add_msg_to_log("sa\n", log);
 	}
 }
 
@@ -54,31 +83,4 @@ static void	move_all_items_from_b_ordered(t_pswap *pswap, char **log)
 		add_msg_to_log("pa\n", log);
 		try_swap_a(pswap, log);
 	}
-}
-
-char	*pswap_sort(t_pswap *pswap)
-{
-	char	*log;
-	size_t	sentinel;
-	size_t	size;
-
-	log = NULL;
-	if (!pswap || pswap->a->size < 2)
-		return (log);
-	sentinel = 1;
-	size = pswap->a->size;
-	while (sentinel <= size)
-	{
-		try_rotate_up_a(pswap, &log);
-		try_swap_a(pswap, &log);
-		if (!stack_pos_ordered(pswap->a, sentinel))
-		{
-			move_n_items_from_a(pswap, sentinel - 1, &log);
-			try_rotate_up_a(pswap, &log);
-			try_swap_a(pswap, &log);
-			move_all_items_from_b_ordered(pswap, &log);
-		}
-		sentinel++;
-	}
-	return (log);
 }
